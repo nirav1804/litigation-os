c3ba949beb0f8b0c9eb3bf04df15a8522d413e15
apple <apple@pool-100-0-145-143.bstnma.east.verizon.net>
2026-04-22 13:38:57 +0530

    fix: remove invalid radix-ui packages causing Vercel install failure

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"l0U6qdywgd7duR+AEvq6Jbvm7dkxJX1zgPutLypmjA0=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"t85C4qHBkZjpVPpvp+O/zQdI8ExeV5OuzbkVEjg+fOY=\"}"
